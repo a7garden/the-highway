c6e215d93c771167979056be63231557fdf3ec54
a7garden <a7garden@icloud.com>
2026-04-13 20:05:15 +0900

    asset: add polyperfect

---
+++ Assets/polyperfect/Low Poly Epic City/- Scripts/SpawnObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject[] spawnObjects;
+    public bool spawn = false;
+    [Range(0.2f,10)]
+    public float interval = 1;
+    private int index = 0;
+    public int maxSpawnCount = -1;
+    private int currentSpawnCount = 0;
+
+    private void Start()
+    {
+        if (spawn)
+        {
+            InvokeRepeating("Spawn", interval, interval);
+        }
+    }
+
+    private void Spawn()
+    {
+        GameObject.Instantiate(spawnObjects[index], transform.position, Quaternion.LookRotation(transform.forward));
+        index++;
+        if (index == spawnObjects.Length)
+            index = 0;
+        if (maxSpawnCount > 0)
+        {
+            if(++currentSpawnCount == maxSpawnCount)
+            {
+                CancelInvoke("Spawn");
+            }
+        }
+    }
+}
